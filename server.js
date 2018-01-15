fe993f6931911a241d6ae7a47c5d56f4319c9639
li li <li li>
2018-01-15 16:52:22 +0800

    port

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get('/', function (req, res) {
   
 });
 
-var server = app.listen(80, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
